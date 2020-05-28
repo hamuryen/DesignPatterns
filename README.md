45bdd850be00108b644d171b2b15ce49432de3b5
hamuryen <burak@hamuryen.com>
2020-05-28 19:16:17 +0300

    no message

--- README.md
+++ README.md
@@ -3,12 +3,12 @@ Sample codes with c++ to understand design patterns.
 This repository contains examples for the following patterns.
 
 #### Creational
-- [Singleton](src/singleton/README.md)
-- [Factory](src/factory/README.md)
+- [Singleton](src/singleton)
+- [Factory](src/factory)
 
 #### Structural
-- [Composite](src/composite/README.md)
-- [Decorator](src/decorator/README.md)
+- [Composite](src/composite)
+- [Decorator](src/decorator)
 
 ### Build
 After repository cloning, create the build files according to the platform you used with CMake and then build them according to your environment. A sample application will be built for each pattern.
--- src/singleton/Singleton.cpp
+++ src/singleton/Singleton.cpp
@@ -34,11 +34,14 @@ DesignPatterns::Singleton::Singleton()
 
 DesignPatterns::Singleton* DesignPatterns::Singleton::GetInstance()
 {
-	//Provides security for multi-thread use to keep object as singleton. So this part is important and necessary. 
-	//If you test below line by making a comment line, you will see that object can create more than once in the the console like "Singleton object created!".
-	std::lock_guard<std::mutex> lg(m_InstanceMutex);
+	//Provides security for multi-thread use to keep object as singleton. So this part is important and necessary. 	
 	if (m_Instance == nullptr)
-		m_Instance = new Singleton();
+	{
+		//If you test below line by making a comment line, you will see that object can create more than once in the the console like "Singleton object created!".
+		std::lock_guard<std::mutex> lg(m_InstanceMutex);
+		if (m_Instance == nullptr)
+			m_Instance = new Singleton();
+	}
 	return m_Instance;
 }
 
